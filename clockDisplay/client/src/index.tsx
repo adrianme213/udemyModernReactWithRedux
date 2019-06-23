ff48436d7950b5594128ff5cad616b8879f07471
Adrian Meza <adrianme213@gmail.com>
2019-06-22 18:16:16 -0700

    clockDisplay: add clockDisplay component with bugs to fix integrating with TypeScript (@adrianme213)

---
+++ clockDisplay/client/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ClockDisplay } from './components/ClockDisplay';
+
+ReactDOM.render(
+  <ClockDisplay />, document.getElementById('root')
+);
