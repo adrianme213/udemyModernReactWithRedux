339916309cabb42b9888f867eac45f0dcff25c17
Adrian Meza <adrianme213@gmail.com>
2019-06-23 01:32:21 -0700

    addUseStateHook: add todo-list functionality to app component with Hooks (@adrianme213)

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,14 +1,63 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-export default function App(): JSX.Element {
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+export default function App():JSX.Element {
+  const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e:FormElem):void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+  }
+
+  const addTodo = (text: string):void => {
+    const newTodos:ITodo[] = [...todos, {text, complete: false}];
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (index:number):void => {
+    const newTodos:ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (index:number):void => {
+    const remainingTodos:ITodo[] = [...todos].filter((todo, i) => i !== index);
+    setTodos(remainingTodos);
+  }
+
   return (
     <Fragment>
       <h1>Todo List</h1>
-      <form>
-        <input type="text" required/>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} required/>
         <button type="submit">Add Todo</button>
       </form>
+      <section>
+        {todos.map((todo:ITodo, index:number) => {
+          return (
+            <Fragment key={index}>
+              <div style={{ textDecoration: todo.complete ? 'line-through' : ''}}>{todo.text}</div>
+              <button 
+                type="button" 
+                onClick={() => completeTodo(index)}>{todo.complete ? 'Incomplete' : 'Complete'}
+              </button>
+              <button
+                type="button"
+                onClick={() => deleteTodo(index)}>Delete {todo.text}
+              </button>
+            </Fragment>
+            )
+        })}
+      </section>
     </Fragment>
   );
 }
